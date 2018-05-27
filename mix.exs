8ca89b4874387efa2093d8cbb37db53370254c9f
Bing Han <h.bing612@gmail.com>
2018-05-27 11:22:01 +0800

    feat(test): add excoveralls

--- mix.exs
+++ mix.exs
@@ -7,6 +7,8 @@ defmodule HTTP2.MixProject do
       version: "0.1.0",
       elixir: "~> 1.6",
       start_permanent: Mix.env() == :prod,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.html": :test],
       deps: deps()
     ]
   end
@@ -21,8 +23,7 @@ defmodule HTTP2.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
+      {:excoveralls, "~> 0.8", only: :test},
     ]
   end
 end
